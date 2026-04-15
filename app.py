7ece86c240bb33e93fe7f41b2f6d1afa63a3259f
kunvariyadk <kunvariya.dk@gmail.com>
2026-04-15 12:36:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-from pyngrok import ngrok
 from base import app
 import os
 
 if __name__ == '__main__':
-    ngrok.set_auth_token("39KWOjEbDyA3A1lGxjeboLbjkXJ_7KjDLkPEvSiYGu4ZJEob8")
-
-    # Only run ngrok in the main process (not Flask reloader child)
-    if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
-        try:
-            ngrok.kill()  # Kill any leftover ngrok processes
-        except Exception:
-            pass
-
-        public_url = ngrok.connect(5000)
-        print(f" Public ngrok URL: {public_url}")
-        app.config["BASE_URL"] = public_url
-
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
